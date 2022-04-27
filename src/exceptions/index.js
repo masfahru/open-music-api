0f85540cca9c24d1191c781d6ee39105971d7613
Imam Fahrur Rofi <imam@fahru.my.id>
2022-04-27 11:11:22 +0700

    Moving local modules to src folder

--- src/exceptions/index.js
+++ src/exceptions/index.js
@@ -1,5 +1,6 @@
 const ClientError = require('./ClientError');
 const InvariantError = require('./InvariantError');
 const NotFoundError = require('./NotFoundError');
+const AuthenticationError = require('./AuthenticationError');
 
-module.exports = { ClientError, InvariantError, NotFoundError };
+module.exports = { ClientError, InvariantError, NotFoundError, AuthenticationError };
